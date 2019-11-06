da31f040a5b021366337b60f8e361474d88bc1ec
Kevin Oliveira <kevinols08@gmail.com>
2019-11-06 10:15:28 -0300

    Validação na API

--- back/app/Http/Controllers/UserController.php
+++ back/app/Http/Controllers/UserController.php
@@ -15,9 +15,15 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $dados = $request->all();
-        if(Funcionario::where('cpf', '=', $request['cpf'])->get()){
-            return response()->json(['erro' => 'CPF já cadastrado']);
-        }        
+	if (Funcionario::where('email', '=', $dados['email'])->exists()) {
+		return response()->json(['erro' => 'e-mail já cadastrado'],500);
+	}
+	if (Funcionario::where('cpf', '=', $dados['cpf'])->exists()) {
+		return response()->json(['erro' => 'CPF já cadastrado'],500);
+	}
+	if (Funcionario::where('usuario', '=', $dados['usuario'])->exists()) {
+		return response()->json(['erro' => 'Nome de usuário já cadastrado'],500);
+	}
         $dados['usuario'] = strtolower($dados['usuario']);
         $dados['password'] = bcrypt($dados['password']);
         $funcionario = Funcionario::create($dados);
@@ -26,6 +32,18 @@ class UserController extends Controller
     public function update(Request $request, Funcionario $funcionario)
     {
         $dados = $request->all();
+	$func = Funcionario::where('email', '=', $dados['email'])->where('id','!=', $funcionario->id)->first();
+	if ($func !== null) {
+		return response()->json(['erro' => 'e-mail já cadastrado'],500);
+	}
+	$func = Funcionario::where('cpf', '=', $dados['cpf'])->where('id','!=', $funcionario->id)->first();
+	if ($func !== null) {
+		return response()->json(['erro' => 'CPF já cadastrado'],500);
+	}
+	$func = Funcionario::where('usuario', '=', $dados['usuario'])->where('id','!=', $funcionario->id)->first();
+	if ($func !== null) {
+		return response()->json(['erro' => 'Nome de usuário já cadastrado'],500);
+	}
         if (array_key_exists('password', $dados)) $dados['password'] = bcrypt($dados['password']);
         $funcionario->update($dados);
         return response()->json($funcionario, 200);
