5dbcf4d44629ab32345980562335859134610853
ViniciusQueiroz-VQ <viniciusqueiroz.voq@gmail.com>
2019-11-09 20:03:50 -0300

    Importação de bibliotecas e tabela atualizada

--- back/app/Models/TelefoneCliente.php
+++ back/app/Models/TelefoneCliente.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TelefoneCliente extends Model
 {
     use SoftDeletes;
 
-    protected $table = 'telefones';
+    protected $table = 'telefones_cliente';
 
     public function cliente(){
         return $this->belongsTo(Cliente::class);
--- back/app/Models/TelefoneFuncionario.php
+++ back/app/Models/TelefoneFuncionario.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TelefoneFuncionario extends Model
 {
     use SoftDeletes;
 
-    protected $table = 'telefones';
+    protected $table = 'telefones_funcionario';
 
     public function usuario(){
         return $this->belongsTo(User::class);
