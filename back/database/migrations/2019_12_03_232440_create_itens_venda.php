2fe3c1eca7e02cff836e34199616bd21ed46be35
Kevin Oliveira <kevinols08@gmail.com>
2019-12-05 15:30:35 -0300

    Correção do nome da classe

--- back/database/migrations/2019_12_03_232440_create_itens_venda.php
+++ back/database/migrations/2019_12_03_232440_create_itens_venda.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateIntemVendas extends Migration
+class CreateItensVendas extends Migration
 {
     /**
      * Run the migrations.
